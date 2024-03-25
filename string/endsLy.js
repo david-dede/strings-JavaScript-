9e40f363fd9ed1ad1b28177735edcd8bcef87961
david-dede <davidenedede@gmail.com>
2024-03-25 14:51:36 +0100

    Update endsLy.js

--- string/endsLy.js
+++ string/endsLy.js
@@ -10,8 +10,8 @@
 
 function endsLy(str) {
   if (
-    // str.charAt(str.length - 1) === "y" &&
-    str.charAt(str.length - 2) === "l"
+    str.charAt(str.length - 1) === "y" &&
+    // str.charAt(str.length - 2) === "l"
   ) {
     return true;
   }
